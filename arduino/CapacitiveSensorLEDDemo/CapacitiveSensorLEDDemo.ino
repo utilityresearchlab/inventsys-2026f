fd0987b3533ad7ed42c4601a610cb5ba0a9cf170
Michael Rivera <mrivera.lee@gmail.com>
2026-09-09 19:16:37 -0600

    update code from LED example

--- arduino/CapacitiveSensorLEDDemo/CapacitiveSensorLEDDemo.ino
+++ arduino/CapacitiveSensorLEDDemo/CapacitiveSensorLEDDemo.ino
@@ -1,9 +1,15 @@
+// Turns an LED on when a capacitve sensor is touched
+// Maintains the state until the touch is ended and a new touch
+// is performed
+
+// touch value for the capacitive sensor 
 #define TOUCH_ACTIVATED_VALUE 500
 
-const int LED_PIN = 27; 
 const int TOUCH_PIN = 32; 
-int counter = 0;
-bool isSensorTouched = false;
+const int LED_PIN = 27; 
+
+bool wasSensorTouched = false;
+bool isLEDOn = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,17 +19,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // Serial.print("Hello: ");
-  // Serial.println(counter);
-  // counter += 1;
   int sensedValue = touchRead(TOUCH_PIN);
+  bool isCurrentlyTouched = (sensedValue <= TOUCH_ACTIVATED_VALUE) ? true : false;
+  // If the sensor is touched, and it was previously not touched
+  if (isCurrentlyTouched && wasSensorTouched != isCurrentlyTouched) {
+    // Update the touch state
+    wasSensorTouched = isCurrentlyTouched;
+    
+    // Toggle LED state
+    isLEDOn = !isLEDOn;
+  }
+
+  // Update LED 
+  digitalWrite(LED_PIN, (isLEDOn) ? HIGH : LOW);
+
+  // Print state
   Serial.print("0,2000");
   Serial.print(",");
   Serial.println(sensedValue);
-  
-  isSensorTouched = (sensedValue <= TOUCH_ACTIVATED_VALUE) ? true : false;
-
-  // Update LED State
-  digitalWrite(LED_PIN, (isSensorTouched) ? HIGH : LOW);
   delay(10);
 }
